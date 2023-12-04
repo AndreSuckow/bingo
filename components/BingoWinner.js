fd78e939c7e36b616e86a76f073f2ffad35baacd
André Luis Suckow <alskw0911@gmail.com>
2023-12-04 16:46:19 -0300

    botão next game + lógica

--- components/BingoWinner.js
+++ components/BingoWinner.js
@@ -1,9 +1,22 @@
 import styles from "../styles/BingoWinner.module.css";
+import { useRouter } from "next/router";
+let RodadaBingo = 0;
 
 export default function BingoWinner(props) {
+  const router = useRouter();
+  const { room, name } = router.query;
+  console.log(room);
+  console.log(name);
+  RodadaBingo++;
+  console.log(RodadaBingo);
   return (
     <section className={styles.main}>
       <p className={styles.title}>{props.winner} venceu!</p>
+      <button className={styles.btnNextGame}>
+        <a href={`http://localhost:3000/bingo${RodadaBingo}?name=${name}`}>
+          Próximo jogo
+        </a>
+      </button>
     </section>
   );
 }
